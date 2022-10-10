abcf0153dc7d4222874ef520c1b1c59b33bbfccd
Martins100-Tmd <olaitanfabayo@gmail.com>
2022-10-10 02:09:35 -0700

    Sorting Algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,37 @@
 #include "sort.h"
+
 /**
- * bubble_sort - bubble sort an array
- * @array: array to be sorted
- * @size: size of array
- * Return: void
+ * bubble_sort - a sorting algorithm that sorts arrays
+ * @array: the array to be sorted
+ * @size: size of the array to be sorted
+ *
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, flag;
+	size_t i, j, k;
+	int temp;
+	int flag = 1;
 
-	if (size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size && flag == 1; i++)
 	{
 		flag = 0;
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				for (k = 0; array && k < size; k++)
+				{
+					if (k > 0)
+						printf(", ");
+					printf("%d", array[k]);
+				}
+				printf("\n");
 				flag = 1;
-				print_array(array, size);
 			}
 		}
-		if (flag == 1)
-			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -1,10 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
-/* Standard Library*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,12 +15,18 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-void swap(int *x, int *y);
+
+
+/* prototypes for our functions */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+
+/* prototype for test functions used by checker */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
 #endif
